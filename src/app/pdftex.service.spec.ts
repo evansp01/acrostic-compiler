e4605594458f8a773ca1bcb62be648a043d90508
Evan Palmer <evansp01@gmail.com>
2024-08-10 12:57:16 +0100

    fix test errors

--- src/app/pdftex.service.spec.ts
+++ src/app/pdftex.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { LatexService } from './pdftex.service';
+import { PdfTex } from './pdftex.service';
 
 describe('LatexService', () => {
-  let service: LatexService;
+  let service: PdfTex;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(LatexService);
+    service = TestBed.inject(PdfTex);
   });
 
   it('should be created', () => {
